470b316dac560339add2e7a11d97f1f8d725f89a
anilkumarsheoran <anilkumar.sheoran@gmail.com>
2016-09-22 16:14:12 +0530

    second commit

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'abc',
+	template:'<div>{{pagetitle}}</div>'
+
+
+})
+
+
+
+
+export class AppComponent{
+	pagetitle:string='Anil '
+}
